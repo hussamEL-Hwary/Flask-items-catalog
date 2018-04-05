59261aec1b942f3218fd78ee2c24ed99a0352900
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2018-04-05 21:01:14 +0200

    Feat: add user table

---
+++ model.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column,Integer,String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), index=True)
+    email = Column(String)
+    password = Column(String(64))
+
+    def hash_password(self, password):
+        self.password = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password)
