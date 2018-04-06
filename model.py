af6fe7dc05f0d367c3f4ae018b92c7bfd9eb7595
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2018-04-06 05:59:42 +0200

    Fix: add created_time to item class

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column,Integer,String
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.sql import func
 from passlib.apps import custom_app_context as pwd_context
 
 
@@ -33,6 +34,7 @@ class Item(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String(32), nullable=False)
     description = Column(String, nullable=False)
+    time_created = Column(DateTime(timezone=True), server_default=func.now())
     user_id = Column(Integer, ForeignKey('user.id'))
     category_id = Column(Integer, ForeignKey('category.id'))
     user = relationship(User)
