031f7863fd6c572b028bbee2ac6e403efdd672ec
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2018-04-06 06:08:13 +0200

    Feat: get categories and latest items

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model import Base, User, Category, Item
+from category_controller import CategoryController
+from item_controller import ItemController
+
 
 app = Flask(__name__)
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+category_controller = CategoryController()
+item_controller = ItemController()
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('home.html',
+                           categories=category_controller.get_categories,
+                           latest_items=item_controller.get_latest_items)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.secret_key = 'secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=8000)
