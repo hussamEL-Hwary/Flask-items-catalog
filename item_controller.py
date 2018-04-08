80d61350ee383e1ab2f4e83f912c7b441b5e3447
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2018-04-08 19:45:56 +0200

    Feat: get all items in specific category

--- item_controller.py
+++ item_controller.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
 from model import Base, Item
+from category_controller import CategoryController
 
 
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
+category_controller = CategoryController()
 session = DBSession()
 
 
@@ -14,3 +16,7 @@ class ItemController():
     def get_latest_items(self):
         return session.query(Item).order_by(desc(Item.time_created))\
             .limit(10)
+
+    def get_items_in_category(self, category):
+        return session.query(Item).filter_by(
+            category_id=category_controller.get_category_id(category)).all()
