f8d508b5f85c509e32b1dff9a2ea3aa79d9c969e
vagrant <vagrant@vagrant.vm>
2018-04-15 11:10:10 +0000

    Feat: validate if user email exist in db

---
+++ validator_util.py
@@ -0,0 +1,25 @@
+from wtforms.validators import ValidationError
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model import Base, User
+
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+class Unique(object):
+    """Unique class that used in form validation to
+       check if element is in db or not
+    """
+    def __init__(self, model, field, message=u'This element already exists.'):
+        self.model = model
+        self.field = field
+        self.message = message
+
+    def __call__(self, form, field):
+        check = session.query(self.model).filter(self.field == field.data).first()
+        if check:
+            raise ValidationError(self.message)
