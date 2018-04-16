4fab81b8ed20d4e5b73ec97edb411dcbdfd1311a
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2018-04-16 13:36:57 +0200

    Feat: login user by email and password

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask, render_template, request, flash, jsonify, abort, redirect, url_for
+# flask
+from flask import Flask, render_template, request, flash,\
+    jsonify, abort, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_required,\
+    login_user, logout_user, current_user
+from urlparse import urlparse, urljoin
+from flask import make_response
+import requests
+import random
+import string
+import httplib2
+import json
+
+# forms
+from forms import SignupForm, LoginForm
+
+# controllers
+from category_controller import CategoryController
+from item_controller import ItemController
+from user_controller import UserController
+
+# sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model import Base, User, Category, Item
-from category_controller import CategoryController
-from item_controller import ItemController
-from forms import SignupForm
-from user_controller import UserController
+
+# oauth
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+
 
 app = Flask(__name__)
 engine = create_engine('sqlite:///catalog.db')
@@ -15,20 +37,61 @@ session = DBSession()
 category_controller = CategoryController()
 item_controller = ItemController()
 user_controller = UserController()
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# google client secret
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+
+
+# check if url is safe
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and \
+           ref_url.netloc == test_url.netloc
+
+
+# load user
+@login_manager.user_loader
+def load_user(user_id):
+    return session.query(User).get(user_id)
 
 
 # sign up user
-
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if current_user.is_authenticated:
+        flash("you are logged in")
+        return redirect(url_for('home'))
+
     signup_form = SignupForm(request.form)
     if request.method == 'POST' and signup_form.validate():
         user = User(username=signup_form.name.data, email=signup_form.email.data)
         user.hash_password(signup_form.password.data)
         user_controller.create_user(user)
         return redirect(url_for('home'))
-
     return render_template('signup.html', form=signup_form)
+
+
+# login user by email and password
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        flash("you are logged in")
+        return redirect(url_for('home'))
+
+    login_form = LoginForm(request.form)
+    if request.method == 'POST' and login_form.validate():
+        login_user(login_form.user)
+        flash('successfully logged in')
+        next = request.args.get('next')
+
+        if not is_safe_url(next):
+            abort(404)
+        return redirect(next or url_for('home'))
+    return render_template('login.html', form=login_form)
 
 
 @app.route('/')
