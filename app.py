8ba0b64dc5045679ee073408fd230a88553b98dd
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2018-04-16 13:38:54 +0200

    Feat: login user by google

--- app.py
+++ app.py
@@ -94,6 +94,75 @@ def login():
     return render_template('login.html', form=login_form)
 
 
+# google login
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+    data = answer.json()
+
+    # create new user if it isn't exist
+    username = data['name']
+    email = data['email']
+    user = user_controller.get_user_by_email(email)
+    if user is None:
+        user = User(username=username, email=email)
+        random_password = ''.join(random.choice(string.ascii_uppercase+string.digits)
+                                  for x in xrange(16))
+        user.hash_password(random_password)
+        user_controller.create_user(user)
+
+    # log user in
+    login_user(user)
+    flash("you are now logged in")
+
+    # redirect to home page
+    return redirect(url_for('home'))
+
+
 @app.route('/')
 def home():
     return render_template('home.html',
