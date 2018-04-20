f306f1e7898efd78b18d3a52924249a836e656a5
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2018-04-20 03:08:02 +0200

    Feat: add new item form

--- forms.py
+++ forms.py
@@ -1,13 +1,17 @@
-from wtforms import Form, BooleanField, StringField, validators, PasswordField
-from wtforms.validators import DataRequired, Email, Length, Required, EqualTo
+from wtforms import Form, BooleanField, StringField,\
+    validators, PasswordField, SelectField, TextAreaField
+from wtforms.validators import DataRequired, \
+    Email, Length, Required, EqualTo
 from validator_util import Unique
 from model import User
 from user_controller import UserController
-
+from category_controller import CategoryController
 
 user_controller = UserController()
+category_controller = CategoryController()
 
 
+# sign up form
 class SignupForm(Form):
     """class SignupForm that contains info required to sign up new user
     Attributes:
@@ -15,7 +19,6 @@ class SignupForm(Form):
         email: user email
         password: user password
         confirm: user password confirmation
-
     """
     name = StringField(
         'name', validators=
@@ -38,7 +41,13 @@ class SignupForm(Form):
     confirm = PasswordField('password repeat')
 
 
+# login form
 class LoginForm(Form):
+    """class LoginForm that contains info required to login user
+    Attributes:
+        name: name of user
+        email: user email
+    """
     email = StringField(
         'email', validators=
         [Required(message="email Required"),
@@ -67,3 +76,28 @@ class LoginForm(Form):
 
         self.user = user
         return True
+
+
+# new item form
+class NewItemForm(Form):
+    """class NewItemForm that contains info required to add new item
+        Attributes:
+            title: item title
+            description: item description
+            category: category that item belongs to
+        """
+    title = StringField(
+        'title', validators=
+        [DataRequired(message="title required"),
+         Length(min=5,
+                message="title must be at least 5 characters")])
+
+    description = TextAreaField(
+        'description', validators=
+        [DataRequired(message="description required"),
+         Length(min=10,
+                message="description must be at least 10 characters")])
+
+    category = SelectField(
+        'category', validators=
+        [DataRequired(message="select category")], coerce=int)
